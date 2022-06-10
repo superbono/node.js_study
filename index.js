0687e3b2aa30256306735cc66e37ac22dede6037
superbono <choiar@pincar.co.kr>
2022-06-11 02:58:36 +0900

    register api 테스트

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+const bodyParser = require('body-parser');
 const port = 4000
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlcoded
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// application/json
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://choi:1234aa@testpjt.lqy406d.mongodb.net/?retryWrites=true&w=majority', {
+     useNewUrlParser: true, 
+     useUnifiedTopology: true, 
+}).then(() => console.log('MongoDB Connected...')).catch(
+    err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',(req, res) => {
+
+    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) {
+            return res.json({ success: false, err});
+        }
+        return res.status(200).json({
+            success: true
+        })
+    });
+
 })
 
 app.listen(port, () => {
