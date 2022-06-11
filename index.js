eb942a9ccb65359105d0b971a3ea6a3b7b10634b
superbono <choiar@pincar.co.kr>
2022-06-12 02:28:37 +0900

    권한체크 로직 적용

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const port = 4000
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 
 const { User } = require("./models/User");
 
@@ -24,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!fffffss11')
 })
 
-app.post('/register',(req, res) => {
+app.post('/api/users/register',(req, res) => {
 
     // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
     const user = new User(req.body);
@@ -41,7 +42,7 @@ app.post('/register',(req, res) => {
 
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
         // 로그인 할 때 필요한 로직 
         // 1. 데이터베이스에 요청한 아이디(email) 찾기
          User.findOne({ email: req.body.email }, (err, user) => {
@@ -64,7 +65,7 @@ app.post('/login', (req, res) => {
                 
                 // 토큰을 저장한다. ex) localStorage, cookie, session 등...
                 // 쿠키에 저장시 cookie-parser 라이브러리 설치 
-                res.cookie("data_auth", user.cookie)
+                res.cookie("user_auth", user.cookie)
                     .status(200)
                     .json({
                         loginSuccess: true,
@@ -79,8 +80,24 @@ app.post('/login', (req, res) => {
 
     })
 
+})
 
 
+// 권한을 체크하려면 미들웨어를 만들어야 권한에 따라 페이지를 보여줄 수 있다.
+// 예를들어, 회원가입을 하지 않은 유저라면 권한체크 시에 notUser와 같은 value를 넣어서,
+// 해당 페이지를 이용할 수 없도록 중간에서 해주어야 한다.
+app.get('/api/users/auth', auth ,(req, res) => {
+    // 미들웨어에서 에러없이 이 페이지 왔다는 건 Authentication이 true라는 것이다.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    });
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -60,15 +60,34 @@ userSchema.methods.comparedPassword = function(plainPassword, cb) {
         cb(null, isMatch)
     });
 }
+
 userSchema.methods.generateToken = function(cb) {
     let user = this;
     // jsonwebtoken 라이브러리 설치
     let token = jwt.sign(user._id.toHexString(), 'secretToken');
     user.token = token
+    console.log("토큰" + user.token);
     user.save(function(err, user){
         if(err) return cb(err);
         cb(null, user);
     })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    let user = this;
+    
+    // 토큰을 복호화한다.
+    jwt.verify(token, 'secretToken', function(err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾는다.
+        // 클라이언트에서 가져온 token과 db에 있는 token이 일치하는지 확인한다.
+
+        user.findOne({ "_id": decoded, "token": token }, function(err,user) {
+            if(err) return cb(err);
+            cb(null, user);
+        });
+
+    });
+
 }
 
 const User = mongoose.model('User', userSchema);
