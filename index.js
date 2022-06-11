ddf349402f39a1c97468d7ad38b92e1a4e6a63ee
superbono <choiar@pincar.co.kr>
2022-06-12 01:01:01 +0900

    jsonwebtoken, cookie-parser 라이브러리 설치 비밀번호 암호화, cookie저장, 로그인 유효성 체크

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const port = 4000
 const config = require('./config/key');
 
@@ -9,6 +10,7 @@ const { User } = require("./models/User");
 // application/x-www-form-urlcoded
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 // application/json
 const mongoose = require('mongoose')
@@ -26,7 +28,7 @@ app.post('/register',(req, res) => {
 
     // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
     const user = new User(req.body);
-    
+
 
     user.save((err, userInfo) => {
         if(err) {
@@ -36,6 +38,48 @@ app.post('/register',(req, res) => {
             success: true
         })
     });
+
+})
+
+app.post('/login', (req, res) => {
+        // 로그인 할 때 필요한 로직 
+        // 1. 데이터베이스에 요청한 아이디(email) 찾기
+         User.findOne({ email: req.body.email }, (err, user) => {
+            if(!user) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "실패! 해당하는 이메일이 없습니다."
+                })
+            } 
+        // 2. 데이터베이스에 요청한 아이디(email)이 있다면 비밀번호가 같은지 확인
+        user.comparedPassword(req.body.password, (err, isMatch) => {
+            if(!isMatch) return res.json({
+                loginSuccess: false,
+                message: "비밀번호가 일치하지 않습니다."
+            })
+
+            // 3. 비밀번호가 같다면 Token을 생성
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                
+                // 토큰을 저장한다. ex) localStorage, cookie, session 등...
+                // 쿠키에 저장시 cookie-parser 라이브러리 설치 
+                res.cookie("data_auth", user.cookie)
+                    .status(200)
+                    .json({
+                        loginSuccess: true,
+                        message: "로그인 되었습니다.",
+                        userId: user._id
+                    })
+
+            })
+
+        })
+
+
+    })
+
+
 
 })
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -14,7 +15,7 @@ const userSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        maxlength: 50
+        maxlength: 150
     },
     role: {
         type: Number,
@@ -31,8 +32,8 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre('save', function(next){
 
-    var user = this;
-    if(user.isModified('password')){
+    let user = this;
+    if(user.isModified('password')) {
         // 비밀번호를 암호화 시킨다.
         bcrypt.genSalt(saltRounds, function(err, salt) {
             if(err) return next(err);
@@ -42,9 +43,33 @@ userSchema.pre('save', function(next){
                 next();
             });
         });
+    } else {
+        next();
     }
 
 });
+
+
+userSchema.methods.comparedPassword = function(plainPassword, cb) {
+
+    let user = this;
+    // plainPassword: 클라이언트 화면에서 입력한 패스워드
+    // cb: callback 함수
+    bcrypt.compare(plainPassword, user.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch)
+    });
+}
+userSchema.methods.generateToken = function(cb) {
+    let user = this;
+    // jsonwebtoken 라이브러리 설치
+    let token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
