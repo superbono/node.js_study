24d3724da7453045fe3bb3986ced5a3d5b8c7d69
superbono <choiar@pincar.co.kr>
2022-06-12 00:19:56 +0900

    password를 body에 넣어 데이터를 가져오기 전에 password 암호화하기

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ app.post('/register',(req, res) => {
 
     // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
     const user = new User(req.body);
+    
 
     user.save((err, userInfo) => {
         if(err) {
             return res.json({ success: false, err});
         }
-
         return res.status(200).json({
             success: true
         })
