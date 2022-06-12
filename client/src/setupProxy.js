673eec6115951e99a0214b76ea7e24a27af8f17e
superbono <choiar@pincar.co.kr>
2022-06-12 23:06:38 +0900

    server / client 서버를 동시에 켤 수 있는 concurrently라이브러리 설치

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+// Proxy에서 하는 일: 방화벽 기능, 웹 필터 기능, 캐쉬 데이터, 공유 데이터 제공
+
 module.exports = function(app) {
   app.use(
     '/api',
