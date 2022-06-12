a88c7c8d5c5dcdbde553d2c8027e794fc9994776
superbono <choiar@pincar.co.kr>
2022-06-12 22:32:00 +0900

    CORS 에러

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,7 +4,9 @@ import axios from 'axios';
 const LandingPage = () => {
     
     useEffect(()=>{
-        axios.get('/api/hello')
+        // /api/hello으로만 요청을 보냈을 경우, client포트가 3000이므로 에러 발생
+        // http://localhost:4000/api/hello으로 요청을 보냈을 경우, CORS에러 발생
+        axios.get('http://localhost:4000/api/hello')
              .then(res => console.log(res.data));
     }, []);
     
