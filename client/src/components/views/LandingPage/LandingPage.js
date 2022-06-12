24c4a4bb7fcfe92246e34b0b3964fd4ba37016cb
superbono <choiar@pincar.co.kr>
2022-06-12 22:42:08 +0900

    CORS에러 해결을 위한 http-proxy-middleware 라이브러리 설치

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,9 @@ const LandingPage = () => {
     
     useEffect(()=>{
         // /api/hello으로만 요청을 보냈을 경우, client포트가 3000이므로 에러 발생
-        // http://localhost:4000/api/hello으로 요청을 보냈을 경우, CORS에러 발생
+        // http://localhost:4000/api/hello으로 요청을 보냈을 경우, 
+        // CORS(cross origin resource sharing)에러 발생
+        // 해결하는 방법은 여러가지이지만, proxy를 사용하여 해결한다.
         axios.get('http://localhost:4000/api/hello')
              .then(res => console.log(res.data));
     }, []);
