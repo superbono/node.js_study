69ef0ff45c3fde3181f295c8c41a2301c01d2614
superbono <choiar@pincar.co.kr>
2022-06-12 22:18:10 +0900

    router-dom 설치 및 적용

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,46 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import LandingPage from './views/LandingPage/LandingPage';
+import RegisterPage from './views/RegisterPage/RegisterPage';
+import LoginPage from "./views/LoginPage/LoginPage";
+
+
 function App() {
   return (
-    <div className="App">
-      <h2>ttt</h2>
-    </div>
+<Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/regist">Register</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/regist" element={<RegisterPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
