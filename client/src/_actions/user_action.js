b404faad90d800657e11ea428a3507e7df411746
superbono <choiar@pincar.co.kr>
2022-06-13 16:48:11 +0900

    dispatch -> type 관리 js 추가

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
+import { LOGIN_USER } from './types'
 
 export const loginUser = (dataToSubmit) => {
     const request = axios.post("/api/users/login", dataToSubmit)
                         .then(response => response.data)
 
     return {
-        type: "LOGIN_USER",
+        type: LOGIN_USER,
         payload: request
     }                    
 }
--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,7 +1,12 @@
 
 
-const userReducer = () => {
-
+const userReducer = (state, action) => {
+    switch(action.type) {
+        case "LOGIN_USER": 
+            return "";
+        default:
+            return state;    
+    }
 }
 
 export default userReducer;
