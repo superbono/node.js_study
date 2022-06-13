507aad7725acc6b8312fe7ec86c89b81fcbc3259
superbono <choiar@pincar.co.kr>
2022-06-13 23:16:12 +0900

    로그인페이지 틀 적용

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { useDispatch } from "react-redux"
 import { loginUser } from "../../../_actions/user_action";
 
-const LoginPage = () => {
-    
+const LoginPage = (props) => {
+
     const dispatch = useDispatch();
     const [Email, setEmail] = useState("");
     const [Password, setPassword] = useState("");
-    
+
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
     }
@@ -35,20 +35,20 @@ const LoginPage = () => {
         <div className="LoginPageBox">
             <form className="LoginForm" onSubmit={onSubmit}>
                 <label className="EmailLabel">Email</label>
-                <input 
-                    className="EmailInput" 
+                <input
+                    className="EmailInput"
                     type="email"
                     value={Email}
                     onChange={handleEmailChange}
-                    placeholder="이메일을 입력하세요." 
+                    placeholder="이메일을 입력하세요."
                 />
                 <label className="PasswordLabel">Password</label>
-                <input 
-                    className="PasswordInput" 
-                    type="password" 
+                <input
+                    className="PasswordInput"
+                    type="password"
                     value={Password}
                     onChange={handlePasswordChange}
-                    placeholder="비밀번호를 입력하세요." 
+                    placeholder="비밀번호를 입력하세요."
                 />
                 <div className="BtnContainer">
                     <button className="LoginBtn">로그인</button>
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,8 +1,85 @@
-const RegisterPage = () => {
+import { useState } from "react";
+import { useDispatch } from "react-redux"
+
+
+const RegisterPage = (props) => {
+
+    const [Name, setName] = useState("");
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+    const [ConfirmPassword, setConfirmPassword] = useState("");
+
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleConfirmPasswordChange = (e) => {
+        setConfirmPassword(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+    }
+    const onResetValue = () => {
+        setEmail("");
+        setPassword("");
+    }
     return (
-        <div>
-            RegisterPage
+
+        <div className="LoginPageBox">
+            <form className="LoginForm" onSubmit={onSubmit}>
+                <label className="EmailLabel">Name</label>
+                <input
+                    className="EmailInput"
+                    type="text"
+                    value={Name}
+                    onChange={handleNameChange}
+                    placeholder="이름을 입력하세요."
+                />
+                <label className="EmailLabel">Email</label>
+                <input
+                    className="EmailInput"
+                    type="email"
+                    value={Email}
+                    onChange={handleEmailChange}
+                    placeholder="이메일을 입력하세요."
+                />
+                <label className="PasswordLabel">Password</label>
+                <input
+                    className="PasswordInput"
+                    type="password"
+                    value={Password}
+                    onChange={handlePasswordChange}
+                    placeholder="비밀번호를 입력하세요."
+                />
+                <label className="PasswordLabel">Confirm Password</label>
+                <input
+                    className="PasswordInput"
+                    type="password"
+                    value={ConfirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                    placeholder="비밀번호를 입력하세요."
+                />
+                <div className="BtnContainer">
+                    <button className="LoginBtn">회원가입</button>
+                    <button className="ResetBtn" onClick={onResetValue}>취소</button>
+                </div>
+            </form>
         </div>
+
     )
 }
 
