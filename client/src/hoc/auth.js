aa55588ba964a3fefb3fa33c23d32ba2d33fb5e5
superbono <choiar@pincar.co.kr>
2022-06-15 01:00:23 +0900

    hoc 로직 반영 -> admin / 사용자

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from "react-redux"
 import { auth } from '../../../client/src/_actions/user_action';
+import { useNavigate } from "react-router-dom";
 
 const authHoc = (SpecificComponent, option, adminRoute = null) => {
 
@@ -12,10 +13,30 @@ const authHoc = (SpecificComponent, option, adminRoute = null) => {
 
     function AuthenticationCheck(props) {
         const dispatch = useDispatch();
+        const navigate = useNavigate();
 
         useEffect(() => {
             dispatch(auth())
-                .then(response => console.log(response));
+                .then(response => {
+                    // console.log(response)
+
+                    // 로그인 하지 않은 상태
+                    if (response.payload.isAuth === false) {
+                        if (option) {
+                            navigate('/login');
+                        }
+                    } else {
+                        // 로그인 한 상태
+                        if (adminRoute && !response.payload.isAdmin) {
+                            navigate('/')
+                        } else {
+                            if (!option) {
+                                navigate('/')
+                            }
+                        }
+                    }
+
+                });
         }, [])
 
         return (
